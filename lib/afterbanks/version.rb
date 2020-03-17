9f4f0ba64ce95d50843b6698c7544b13b7d3e9b8
Albert Bellonch Llargués <albert@bellonch.com>
2020-03-17 17:53:45 +0100

    Base structure, settings, logging & banks endpoint

---
+++ lib/afterbanks/version.rb
@@ -0,0 +1,3 @@
+module Afterbanks
+  VERSION = "0.1.0"
+end
---
+++ spec/afterbanks/version_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe "Afterbanks::VERSION" do
+  it "is the proper one" do
+    expect(Afterbanks::VERSION).to eq("0.1.0")
+  end
+end
