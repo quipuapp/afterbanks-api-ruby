96cfd7dc5cbdbf6b5113d6597ece8e966699ec03
Albert Bellonch Llargués <albert@bellonch.com>
2020-03-25 15:23:31 +0100

    Improve testing for accounts endpoint regarding cases with second password and OTP

--- spec/afterbanks/resources/account_spec.rb
+++ spec/afterbanks/resources/account_spec.rb
@@ -1,6 +1,62 @@
 require "spec_helper"
 
 describe Afterbanks::Account do
+  shared_examples "proper request and data parsing" do
+    it "does the proper request and returns the proper Afterbanks::Account instances" do
+      accounts = api_call
+
+      expect(accounts.class).to eq(Afterbanks::Collection)
+      expect(accounts.size).to eq(3)
+
+      account1, account2, account3 = accounts
+
+      expect(account1.class).to eq(Afterbanks::Account)
+      expect(account1.product).to eq("ES2720809591124344566256")
+      expect(account1.type).to eq("checking")
+      expect(account1.balance).to eq(1094.12)
+      expect(account1.currency).to eq("EUR")
+      expect(account1.description).to eq("A checking account")
+      expect(account1.iban).to eq("ES2720809591124344566256")
+      expect(account1.is_owner).to be_truthy
+      expect(account1.holders).to eq(
+        [
+          { "role" => 'Admin', "name" => 'Mary', "id" => 1 },
+          { "role" => 'Admin', "name" => 'Liz', "id" => 2 },
+          { "role" => 'Supervisor', "name" => 'John', "id" => 3 }
+        ]
+      )
+
+      expect(account2.class).to eq(Afterbanks::Account)
+      expect(account2.product).to eq("ES8401821618664757634169")
+      expect(account2.type).to eq("checking")
+      expect(account2.balance).to eq(216.19)
+      expect(account2.currency).to eq("EUR")
+      expect(account2.description).to eq("Another checking account")
+      expect(account2.iban).to eq("ES8401821618664757634169")
+      expect(account2.is_owner).to be_truthy
+      expect(account2.holders).to eq(
+        [
+          { "role" => 'Admin', "name" => 'Mary', "id" => 11 }
+        ]
+      )
+
+      expect(account3.class).to eq(Afterbanks::Account)
+      expect(account3.product).to eq("ES9231902434113168967688")
+      expect(account3.type).to eq("loan")
+      expect(account3.balance).to eq(-91.99)
+      expect(account3.currency).to eq("USD")
+      expect(account3.description).to eq("A loan")
+      expect(account3.iban).to eq("ES9231902434113168967688")
+      expect(account3.is_owner).to be_falsey
+      expect(account3.holders).to eq(
+        [
+          { "role" => 'Admin', "name" => 'Sandy', "id" => 12 },
+          { "role" => 'Supervisor', "name" => 'Joe', "id" => 34 }
+        ]
+      )
+    end
+  end
+
   describe "#list" do
     let(:service) { 'a_service' }
     let(:username) { 'a_user' }
@@ -23,67 +79,85 @@ describe Afterbanks::Account do
     }
 
     context "when returning data" do
-      before do
-        stub_request(:post, "https://api.afterbanks.com/V3/").
-          with(body: body).
-          to_return(
-            status: 200,
-            body: response_json(resource: 'account', action: 'list')
-          )
+      context "for a normal case" do
+        before do
+          stub_request(:post, "https://api.afterbanks.com/V3/").
+            with(body: body).
+            to_return(
+              status: 200,
+              body: response_json(resource: 'account', action: 'list')
+            )
+        end
+
+        include_examples "proper request and data parsing"
       end
 
-      it "works" do
-        accounts = api_call
+      context "for a case with a second password" do
+        let(:body) {
+          {
+            "servicekey" => 'a_servicekey_which_works',
+            "service" => service,
+            "user" => username,
+            "pass" => password,
+            "pass2" => "cucamonga",
+            "products" => 'GLOBAL'
+          }
+        }
+        let(:api_call) {
+          Afterbanks::Account.list(
+            service: service,
+            username: username,
+            password: password,
+            password2: 'cucamonga'
+          )
+        }
 
-        expect(accounts.class).to eq(Afterbanks::Collection)
-        expect(accounts.size).to eq(3)
+        before do
+          stub_request(:post, "https://api.afterbanks.com/V3/").
+            with(body: body).
+            to_return(
+              status: 200,
+              body: response_json(resource: 'account', action: 'list')
+            )
+        end
 
-        account1, account2, account3 = accounts
+        include_examples "proper request and data parsing"
+      end
 
-        expect(account1.class).to eq(Afterbanks::Account)
-        expect(account1.product).to eq("ES2720809591124344566256")
-        expect(account1.type).to eq("checking")
-        expect(account1.balance).to eq(1094.12)
-        expect(account1.currency).to eq("EUR")
-        expect(account1.description).to eq("A checking account")
-        expect(account1.iban).to eq("ES2720809591124344566256")
-        expect(account1.is_owner).to be_truthy
-        expect(account1.holders).to eq(
-          [
-            { "role" => 'Admin', "name" => 'Mary', "id" => 1 },
-            { "role" => 'Admin', "name" => 'Liz', "id" => 2 },
-            { "role" => 'Supervisor', "name" => 'John', "id" => 3 }
-          ]
-        )
+      context "for an OTP case" do
+        let(:body) {
+          {
+            "servicekey" => 'a_servicekey_which_works',
+            "service" => service,
+            "user" => username,
+            "pass" => password,
+            "products" => 'GLOBAL',
+            "session_id" => 'abcd1234',
+            "OTP" => '1745',
+            "counterId" => "4"
+          }
+        }
+        let(:api_call) {
+          Afterbanks::Account.list(
+            service: service,
+            username: username,
+            password: password,
+            session_id: 'abcd1234',
+            otp: '1745',
+            counter_id: 4
+          )
+        }
 
-        expect(account2.class).to eq(Afterbanks::Account)
-        expect(account2.product).to eq("ES8401821618664757634169")
-        expect(account2.type).to eq("checking")
-        expect(account2.balance).to eq(216.19)
-        expect(account2.currency).to eq("EUR")
-        expect(account2.description).to eq("Another checking account")
-        expect(account2.iban).to eq("ES8401821618664757634169")
-        expect(account2.is_owner).to be_truthy
-        expect(account2.holders).to eq(
-          [
-            { "role" => 'Admin', "name" => 'Mary', "id" => 11 }
-          ]
-        )
+        before do
+          stub_request(:post, "https://api.afterbanks.com/V3/").
+            with(body: body).
+            to_return(
+              status: 200,
+              body: response_json(resource: 'account', action: 'list')
+            )
+        end
 
-        expect(account3.class).to eq(Afterbanks::Account)
-        expect(account3.product).to eq("ES9231902434113168967688")
-        expect(account3.type).to eq("loan")
-        expect(account3.balance).to eq(-91.99)
-        expect(account3.currency).to eq("USD")
-        expect(account3.description).to eq("A loan")
-        expect(account3.iban).to eq("ES9231902434113168967688")
-        expect(account3.is_owner).to be_falsey
-        expect(account3.holders).to eq(
-          [
-            { "role" => 'Admin', "name" => 'Sandy', "id" => 12 },
-            { "role" => 'Supervisor', "name" => 'Joe', "id" => 34 }
-          ]
-        )
+        include_examples "proper request and data parsing"
       end
     end
 
