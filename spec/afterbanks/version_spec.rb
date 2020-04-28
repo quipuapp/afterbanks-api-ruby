a41be00ffb1da834b7a76d5d03702240e815d462
Albert Bellonch Llargués <albert@bellonch.com>
2020-04-28 16:28:40 +0200

    Fix unupdated spec

--- spec/afterbanks/version_spec.rb
+++ spec/afterbanks/version_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe "Afterbanks::VERSION" do
   it "is the proper one" do
-    expect(Afterbanks::VERSION).to eq("0.1.0")
+    expect(Afterbanks::VERSION).to eq("0.1.1")
   end
 end
