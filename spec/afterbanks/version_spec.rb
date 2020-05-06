226f11683897d50e2a3453d4c48c8ee658b53b92
Albert Bellonch Llargués <albert@bellonch.com>
2020-05-06 17:40:32 +0200

    Bump to 0.2.3

--- spec/afterbanks/version_spec.rb
+++ spec/afterbanks/version_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe "Afterbanks::VERSION" do
   it "is the proper one" do
-    expect(Afterbanks::VERSION).to eq("0.2.2")
+    expect(Afterbanks::VERSION).to eq("0.2.3")
   end
 end
