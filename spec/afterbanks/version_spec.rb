d9dd4a781dba607e4e48469f8038c649ed12f6a0
Albert Bellonch Llargués <albert@bellonch.com>
2023-06-14 13:45:37 +0200

    Bump to 0.4.1

--- spec/afterbanks/version_spec.rb
+++ spec/afterbanks/version_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe "Afterbanks::VERSION" do
   it "is the proper one" do
-    expect(Afterbanks::VERSION).to eq("0.4.0")
+    expect(Afterbanks::VERSION).to eq("0.4.1")
   end
 end
